53f2b473162eabd404e24da4ce933ddfc7bf56dc
Xio <mxbadano@gmail.com>
2025-01-26 14:26:57 -0300

    trying to spawn but no success

--- Enemys/spawn_bubble_bad.gd
+++ Enemys/spawn_bubble_bad.gd
@@ -10,18 +10,23 @@ func _ready():
 	set_process(false)
 
 func start_spawning():
-	set_process(false)
+	set_process(true)
 
 func _process(_delta):
 	spawn()
 
 func spawn():
-	if bool_spawn and not Global.loss:
+	#if bool_spawn and not Global.loss:
+	if bool_spawn:
 		$Timer.start()
 		bool_spawn = false
 		var bubble_instance = bubble_scene.instantiate()
-		bubble_instance.position = Vector2(random.randi_range(1160,1163),random.randi_range(-8,360))
+		if bubble_instance == null:
+			print("Error: Failed to instantiate bubble_scene!")
+			return
+		bubble_instance.position = Vector2(random.randi_range(0,5000),random.randi_range(-8,560))
 		add_child(bubble_instance)
+		print("Spawning bubble at: ", bubble_instance)
 
 func _on_timer_timeout() -> void:
 	bool_spawn = true
